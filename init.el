c2b7c899f80ebecfc6692be863d0bc4d024cc494
Roni Jaakkola <roni.jaakkola@gofore.com>
2018-05-06 11:14:32 +0300

    Initial configuration

---
+++ init.el
@@ -0,0 +1,21 @@
+(package-initialize)
+
+;; Load configuration
+(defun load-config()
+  (interactive)
+  (org-babel-load-file "~/.emacs.d/config.org"))
+
+(load-config)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (auto-complete beacon powerline smex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
